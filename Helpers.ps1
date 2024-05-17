6247e16b22acf270b1990255d2fd3e03a50ac5a4
Y2K4 <y2k04@outlook.com>
2024-05-17 18:24:15 +1000

    Rewrite tiny11builder I have not tested the code yet. So this is at your own risk if you try to use it.

--- Helpers.ps1
+++ Helpers.ps1
@@ -9,3 +9,131 @@ function ElevateIfRequired() {
         exit
     }
 }
+
+function TakeOwnership($path, $tExtraArgs = "", $iExtraArgs = "") {
+    & takeown /f $path $tExtraArgs > $null 2>&1
+    & icacls $path /grant "$($adminGroup.Value):(F)" $iExtraArgs > $null 2>&1
+}
+
+## this function allows PowerShell to take ownership of the Scheduled Tasks registry key from TrustedInstaller. Based on Jose Espitia's script.
+function Enable-Privilege {
+    param(
+        [ValidateSet("SeAssignPrimaryTokenPrivilege", "SeAuditPrivilege", "SeBackupPrivilege", "SeChangeNotifyPrivilege", "SeCreateGlobalPrivilege", "SeCreatePagefilePrivilege", "SeCreatePermanentPrivilege", "SeCreateSymbolicLinkPrivilege", "SeCreateTokenPrivilege", "SeDebugPrivilege", "SeEnableDelegationPrivilege", "SeImpersonatePrivilege", "SeIncreaseBasePriorityPrivilege", "SeIncreaseQuotaPrivilege", "SeIncreaseWorkingSetPrivilege", "SeLoadDriverPrivilege", "SeLockMemoryPrivilege", "SeMachineAccountPrivilege", "SeManageVolumePrivilege", "SeProfileSingleProcessPrivilege", "SeRelabelPrivilege", "SeRemoteShutdownPrivilege", "SeRestorePrivilege", "SeSecurityPrivilege", "SeShutdownPrivilege", "SeSyncAgentPrivilege", "SeSystemEnvironmentPrivilege", "SeSystemProfilePrivilege", "SeSystemtimePrivilege", "SeTakeOwnershipPrivilege", "SeTcbPrivilege", "SeTimeZonePrivilege", "SeTrustedCredManAccessPrivilege", "SeUndockPrivilege", "SeUnsolicitedInputPrivilege")]
+        $Privilege, ## The process on which to adjust the privilege. Defaults to the current process.
+        $ProcessId = $pid, ## Switch to disable the privilege, rather than enable it.
+        [Switch] $Disable
+    )
+    $definition = 'using System;using System.Runtime.InteropServices;public class AdjPriv{[DllImport("advapi32.dll",ExactSpelling=true,SetLastError=true)]internal static extern bool AdjustTokenPrivileges(IntPtr htok,bool disall,ref TokPriv1Luid newst,int len,IntPtr prev,IntPtr relen);[DllImport("advapi32.dll",ExactSpelling=true,SetLastError=true)]internal static extern bool OpenProcessToken(IntPtr h,int acc,ref IntPtr phtok);[DllImport("advapi32.dll",SetLastError=true)]internal static extern bool LookupPrivilegeValue(string host,string name,ref long pluid);[StructLayout(LayoutKind.Sequential,Pack=1)]internal struct TokPriv1Luid{public int Count,Attr;public long Luid;}internal const int SE_PRIVILEGE_ENABLED=0x00000002;internal const int SE_PRIVILEGE_DISABLED=0x00000000;internal const int TOKEN_QUERY=0x00000008;internal const int TOKEN_ADJUST_PRIVILEGES=0x00000020;public static bool EnablePrivilege(long processHandle,string privilege,bool disable){bool retVal;TokPriv1Luid tp;IntPtr hproc=new IntPtr(processHandle);IntPtr htok=IntPtr.Zero;retVal=OpenProcessToken(hproc,TOKEN_ADJUST_PRIVILEGES|TOKEN_QUERY,ref htok);tp.Count=1;tp.Luid=0;if(disable){tp.Attr=SE_PRIVILEGE_DISABLED;}else{tp.Attr=SE_PRIVILEGE_ENABLED;}retVal=LookupPrivilegeValue(null,privilege,ref tp.Luid);retVal=AdjustTokenPrivileges(htok,false,ref tp,0,IntPtr.Zero,IntPtr.Zero);return retVal;}}'
+    $processHandle = (Get-Process -id $ProcessId).Handle
+    $type = Add-Type $definition -PassThru
+    $type[0]::EnablePrivilege($processHandle, $Privilege, $Disable)
+}
+
+$registries = @(
+    ("HKLM\zCOMPONENTS", "$workingDir\scratchdir\Windows\System32\config\COMPONENTS"),
+    ("HKLM\zDEFAULT", "$workingDir\scratchdir\Windows\System32\config\default"),
+    ("HKLM\zNTUSER", "$workingDir\scratchdir\Users\Default\ntuser.dat"),
+    ("HKLM\zSOFTWARE", "$workingDir\scratchdir\Windows\System32\config\SOFTWARE"),
+    ("HKLM\zSYSTEM", "$workingDir\scratchdir\Windows\System32\config\SYSTEM")
+)
+
+$bootRegKeys = [ordered]@{
+    "Add system requirements bypass" = @(
+        ("add", "HKLM\zDEFAULT\Control Panel\UnsupportedHardwareNotificationCache", "SV1", "REG_DWORD", 0),
+        ("add", "HKLM\zDEFAULT\Control Panel\UnsupportedHardwareNotificationCache", "SV2", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\Control Panel\UnsupportedHardwareNotificationCache", "SV1", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\Control Panel\UnsupportedHardwareNotificationCache", "SV2", "REG_DWORD", 0),
+        ("add", "HKLM\zSYSTEM\Setup\LabConfig", "BypassCPUCheck", "REG_DWORD", 1),
+        ("add", "HKLM\zSYSTEM\Setup\LabConfig", "BypassRAMCheck", "REG_DWORD", 1),
+        ("add", "HKLM\zSYSTEM\Setup\LabConfig", "BypassSecureBootCheck", "REG_DWORD", 1),
+        ("add", "HKLM\zSYSTEM\Setup\LabConfig", "BypassStorageCheck", "REG_DWORD", 1),
+        ("add", "HKLM\zSYSTEM\Setup\LabConfig", "BypassTPMCheck", "REG_DWORD", 1),
+        ("add", "HKLM\zSYSTEM\Setup\MoSetup", "AllowUpgradesWithUnsupportedTPMOrCPU", "REG_DWORD", 1)
+    )
+}
+
+$installRegKeys = [ordered]@{
+    "Add system requirements bypass" = @(
+        ("add", "HKLM\zDEFAULT\Control Panel\UnsupportedHardwareNotificationCache", "SV1", "REG_DWORD", 0),
+        ("add", "HKLM\zDEFAULT\Control Panel\UnsupportedHardwareNotificationCache", "SV2", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\Control Panel\UnsupportedHardwareNotificationCache", "SV1", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\Control Panel\UnsupportedHardwareNotificationCache", "SV2", "REG_DWORD", 0),
+        ("add", "HKLM\zSYSTEM\Setup\LabConfig", "BypassCPUCheck", "REG_DWORD", 1),
+        ("add", "HKLM\zSYSTEM\Setup\LabConfig", "BypassRAMCheck", "REG_DWORD", 1),
+        ("add", "HKLM\zSYSTEM\Setup\LabConfig", "BypassSecureBootCheck", "REG_DWORD", 1),
+        ("add", "HKLM\zSYSTEM\Setup\LabConfig", "BypassStorageCheck", "REG_DWORD", 1),
+        ("add", "HKLM\zSYSTEM\Setup\LabConfig", "BypassTPMCheck", "REG_DWORD", 1),
+        ("add", "HKLM\zSYSTEM\Setup\MoSetup", "AllowUpgradesWithUnsupportedTPMOrCPU", "REG_DWORD", 1)
+    )
+    "Disable sponsored apps" = @(
+        ("add", "HKLM\zNTUSER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "OemPreInstalledAppsEnabled", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "PreInstalledAppsEnabled", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "SilentInstalledAppsEnabled", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "ContentDeliveryAllowed", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "FeatureManagementEnabled", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "SoftLandingEnabled", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "SubscribedContentEnabled", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "SubscribedContent-310093Enabled", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "SubscribedContent-338388Enabled", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "SubscribedContent-338389Enabled", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "SubscribedContent-338393Enabled", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "SubscribedContent-353694Enabled", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "SubscribedContent-353696Enabled", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager", "SystemPaneSuggestionsEnabled", "REG_DWORD", 0),
+        ("add", "HKLM\zSOFTWARE\Policies\Microsoft\Windows\CloudContent", "DisableWindowsConsumerFeatures", "REG_DWORD", 1),
+        ("add", "HKLM\zSOFTWARE\Policies\Microsoft\Windows\CloudContent", "DisableConsumerAccountStateContent", "REG_DWORD", 1),
+        ("add", "HKLM\zSOFTWARE\Policies\Microsoft\Windows\CloudContent", "DisableCloudOptimizedContent", "REG_DWORD", 1),
+        ("add", "HKLM\zSOFTWARE\Microsoft\PolicyManager\current\device\Start", "ConfigureStartPins", "REG_SZ", '{"pinnedList": [{}]}'),
+        ("add", "HKLM\zSOFTWARE\Policies\Microsoft\PushToInstall", "DisablePushToInstall", "REG_DWORD", 1),
+        ("add", "HKLM\zSOFTWARE\Policies\Microsoft\MRT", "DontOfferThroughWUAU", "REG_DWORD", 1),
+        ("delete", "HKLM\zNTUSER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager\Subscriptions"),
+        ("delete", "HKLM\zNTUSER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager\SuggestedApps")
+    )
+    "Enable local accounts on OOBE" = @(
+        ("add", "HKLM\zSOFTWARE\Microsoft\Windows\CurrentVersion\OOBE", "BypassNRO", "REG_DWORD", 1)
+    )
+    "Disable reserved storage" = @(
+        ("add", "HKLM\zSOFTWARE\Microsoft\Windows\CurrentVersion\ReserveManager", "ShippedWithReserves", "REG_DWORD", 0)
+    )
+    "Disable chat icon" = @(
+        ("add", "HKLM\zSOFTWARE\Policies\Microsoft\Windows\Windows Chat", "ChatIcon", "REG_DWORD", 3),
+        ("add", "HKLM\zNTUSER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "TaskbarMn", "REG_DWORD", 0)
+    )
+    "Remove Microsoft Edge related keys" = @(
+        ("delete", "HKLM\zSOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge"),
+        ("delete", "HKLM\zSOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge Update")
+    )
+    "Disable OneDrive folder backup" = @(
+        ("add", "HKLM\zSOFTWARE\Policies\Microsoft\Windows\OneDrive", "DisableFileSyncNGSC", "REG_DWORD", 1)
+    )
+    "Disable telemetry" = @(
+        ("add", "HKLM\zNTUSER\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo", "Enabled", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\Software\Microsoft\Windows\CurrentVersion\Privacy", "TailoredExperiencesWithDiagnosticDataEnabled", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\Software\Microsoft\Speech_OneCore\Settings\OnlineSpeechPrivacy", "HasAccepted", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\Software\Microsoft\Input\TIPC", "Enabled", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\Software\Microsoft\InputPersonalization", "RestrictImplicitInkCollection", "REG_DWORD", 1),
+        ("add", "HKLM\zNTUSER\Software\Microsoft\InputPersonalization", "RestrictImplicitTextCollection", "REG_DWORD", 1),
+        ("add", "HKLM\zNTUSER\Software\Microsoft\InputPersonalization\TrainedDataStore", "HarvestContacts", "REG_DWORD", 0),
+        ("add", "HKLM\zNTUSER\Software\Microsoft\Personalization\Settings", "AcceptedPrivacyPolicy", "REG_DWORD", 0),
+        ("add", "HKLM\zSOFTWARE\Policies\Microsoft\Windows\DataCollection", "AllowTelemetry", "REG_DWORD", 0),
+        ("add", "HKLM\zSYSTEM\ControlSet001\Services\dmwappushservice", "Start", "REG_DWORD", 4)
+    )
+    "Delete Application Compatibility Appraiser" = @(
+        ("delete", "HKLM\zSOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tasks\{0600DD45-FAF2-4131-A006-0B17509B9F78}")
+    )
+    "Delete Customer Experience Improvement Program" = @(
+        ("delete", "HKLM\zSOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tasks\{4738DE7A-BCC1-4E2D-B1B0-CADB044BFA81}"),
+        ("delete", "HKLM\zSOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tasks\{6FAC31FA-4A85-4E64-BFD5-2154FF4594B3}"),
+        ("delete", "HKLM\zSOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tasks\{FC931F16-B50A-472E-B061-B6F79A71EF59}")
+    )
+    "Delete Program Data Updater" = @(
+        ("delete", "HKLM\zSOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tasks\{0671EB05-7D95-4153-A32B-1426B9FE61DB}")
+    )
+    "Delete autochk proxy" = @(
+        ("delete", "HKLM\zSOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tasks\{87BF85F4-2CE1-4160-96EA-52F554AA28A2}"),
+        ("delete", "HKLM\zSOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tasks\{8A9C643C-3D74-4099-B6BD-9C6D170898B1}")
+    )
+    "Delete QueueReporting" = @(
+        ("delete", "HKLM\zSOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tasks\{E3176A65-4E44-4ED3-AA73-3283660ACB9C}")
+    )
+}
--- tiny11maker-rewrite.ps1
+++ tiny11maker-rewrite.ps1
@@ -2,8 +2,10 @@ using namespace System.Console
 using namespace System.Diagnostics
 using namespace System.Environment
 using namespace System.Management.Automation
+using namespace System.Security.AccessControl
+using namespace Microsoft.Win32
 
-[Console]::Title = "tiny11 builder"
+[Console]::Title = "tiny11 builder (Dev_17-05-24)"
 #$Host.UI.RawUI.WindowTitle = "tiny11 builder"
 
 if ((Get-ExecutionPolicy) -ne 'Unrestricted') { Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Confirm:$false }
@@ -15,6 +17,7 @@ $newLine = [Environment]::NewLine
 $OSDrive = $env:SystemDrive
 $workingDir = "$PSScriptRoot\tiny11"
 $arch = $Env:PROCESSOR_ARCHITECTURE
+$packagePrefixes = 'Clipchamp.Clipchamp_', 'Microsoft.BingNews_', 'Microsoft.BingWeather_', 'Microsoft.GamingApp_', 'Microsoft.GetHelp_', 'Microsoft.Getstarted_', 'Microsoft.MicrosoftOfficeHub_', 'Microsoft.MicrosoftSolitaireCollection_', 'Microsoft.People_', 'Microsoft.PowerAutomateDesktop_', 'Microsoft.Todos_', 'Microsoft.WindowsAlarms_', 'microsoft.windowscommunicationsapps_', 'Microsoft.WindowsFeedbackHub_', 'Microsoft.WindowsMaps_', 'Microsoft.WindowsSoundRecorder_', 'Microsoft.Xbox.TCUI_', 'Microsoft.XboxGamingOverlay_', 'Microsoft.XboxGameOverlay_', 'Microsoft.XboxSpeechToTextOverlay_', 'Microsoft.YourPhone_', 'Microsoft.ZuneMusic_', 'Microsoft.ZuneVideo_', 'MicrosoftCorporationII.MicrosoftFamily_', 'MicrosoftCorporationII.QuickAssist_', 'MicrosoftTeams_', 'Microsoft.549981C3F5F10_'
 $drives = Get-PSDrive | Select-Object -ExpandProperty 'Root' | Select-String '^[a-z]:\\$' | ForEach-Object {$_ -replace "\\"} | Where-Object { $_ -ne $OSDrive }
 
 New-Item -ItemType Directory -Force -Path "$workingDir\sources" > $null
@@ -28,7 +31,7 @@ if (Get-Module -ListAvailable -Name PSWriteColor) { Import-Module PSWriteColor }
 Clear-Host
 
 function selectDrive() {
-    Write-Host "tiny11 image creator (Dev_16-05-24)$newLine${newLine}Please select drive letter for the Windows 11 image:"
+    Write-Host "$([Console]::Title)$newLine${newLine}Please select drive letter for the Windows 11 image:"
     $drive = Show-Menu $drives
 
     if ((Test-Path "$drive\sources\boot.wim") -eq $false -or (Test-Path "$drive\sources\install.wim") -eq $false) {
@@ -50,14 +53,15 @@ function selectDrive() {
         }
 
         Clear-Host
-        Write-Host "tiny11 image creator (Dev_16-05-24)$newLine${newLine}Converting image to WIM...$newLine${$newLine}"
+        Write-Host "$([Console]::Title)$newLine${newLine}Converting image to WIM...$newLine${$newLine}"
         & dism /Export-Image /SourceImageFile:"$drive\sources\install.esd" /SourceIndex:$index /DestinationImageFile:"$workingDir\sources\install.wim" /Compress:max /CheckIntegrity
     }
 
-    copyFiles
+    copyFiles($removeESDOnCopy)
 }
 
-function copyFiles() {
+function copyFiles($removeESDOnCopy) {
+    Clear-Host
     Write-Progress -Id 0 -Activity "Preparing to build tiny11" -Status "Copying Windows image..." -PercentComplete 0
     do { Copy-ItemWithProgress $drive $workingDir -ProgressID 0 -OutVariable $status } until ($status)
 
@@ -73,7 +77,7 @@ function copyFiles() {
         if ($images.Count -eq 1) {
             $index = 1
         } else {
-            Write-Host "$newLine${$newLine}Please select the image index:"
+            Write-Host "$([Console]::Title)$newLine${$newLine}Please select the image index:"
             $index = Show-Menu $images
         }
     }
@@ -82,29 +86,168 @@ function copyFiles() {
 }
 
 function startBuild() {
+    Clear-Host
+    Write-Host "$([Console]::Title)$newLine${$newLine}"
     $job = Start-Job -Name "startBuild" -ScriptBlock {
-        & takeown /F "$workingDir\sources\install.wim" > $null 2>&1
-        & icacls "$workingDir\sources\install.wim" "/grant" "$($adminGroup.Value):(F)" > $null 2>&1
+        TakeOwnership("$workingDir\sources\install.wim")
         try { Set-ItemProperty -Path "$workingDir\sources\install.wim" -Name IsReadOnly -Value $false -ErrorAction Stop > $null 2>&1}
         catch {} # This block will catch the error and suppress it.
 
         New-Item -ItemType Directory -Force -Path "$workingDir\scratchdir" > $null
         & dism /mount-image /imagefile:"$workingDir\sources\install.wim" /index:$index /mountdir:"$workingDir\scratchdir" | ForEach-Object {
-            Write-Progress -Activity "Mounting install.wim..." -Status $_
+            Write-Progress -Activity "Mounting install image..." -Status $_
         }
 
-        Write-Progress -Activity "Getting default system UI language..."
-        $languageLine = $(& dism /Get-Intl /Image:"$workingDir\scratchdir") -split '\n' | Where-Object { $_ -match 'Default system UI language : ([a-zA-Z]{2}-[a-zA-Z]{2})' }
-
-        if ($languageLine) {
-            $languageCode = $Matches[1]
+        Write-Progress -Activity "Getting default system UI language..." -Status ""
+        $languageCode = $(& dism /Get-Intl /Image:"$workingDir\scratchdir") -split '\n' | Where-Object { if ($_ -match 'Default system UI language : ([a-zA-Z]{2}-[a-zA-Z]{2})') { $matches[1] }}
+        if ($languageCode) {
             Write-Host "Info: Found language code: $languageCode"
         } else {
             Write-Host "Error: Default system UI language code not found."
         }
 
         Write-Progress -Activity "Getting image architecture..."
-        & dism /get-wiminfo /wimfile:"$workingDir\sources\install.wim" /index:$index
+        $imgArch = & dism /get-wiminfo /wimfile:"$workingDir\sources\install.wim" /index:1 | Foreach-Object { if ($_ -match "Architecture : (.*)") { $matches[1] }}
+        if ($imgArch -eq 'x64') {
+            $imgArch = 'amd64'
+            Write-Host "Info: Found image architecture: $imgArch"
+        } elseif (-not $arch) {
+            Write-Host "Error: Image architecture not found."
+        }
+
+        Write-Progress -Activity "Preparing to remove preinstalled packages..." -Status "Getting preinstalled packages"
+        $packages = & dism /image:"$workingDir\scratchdir" /Get-ProvisionedAppxPackages | Foreach-Object { if ($_ -match "PackageName : (.*)") { $matches[1] }}
+        Write-Progress -Status "Selecting packages to remove..."
+        $packagesToRemove = $packages | Foreach-Object { $packagePrefixes -contains "$_" }
+        Write-Progress -Activity "Removing packages..." -Status ""
+        $packagesToRemove | ForEach-Object {
+            Write-Progress -Status "($($packagesToRemove.IndexOf($_) + 1) of $($packagesToRemove.Count + 1)) $_"
+            & dism /image:"$workingDir\scratchdir" /Remove-ProvisionedAppxPackage /PackageName:"$_"  > $null 2>&1
+        }
+        
+        Write-Progress -Status "Removing Microsoft Edge"
+        Remove-Item -Path "$workingDir\scratchdir\Program Files (x86)\Microsoft\Edge" -Recurse -Force > $null 2>&1
+        Remove-Item -Path "$workingDir\scratchdir\Program Files (x86)\Microsoft\EdgeUpdate" -Recurse -Force > $null 2>&1
+        Remove-Item -Path "$workingDir\scratchdir\Program Files (x86)\Microsoft\EdgeCore" -Recurse -Force > $null 2>&1
+        if ($imgArch -eq 'amd64' -or 'arm64') {
+            $folderPath = Get-ChildItem -Path "$workingDir\scratchdir\Windows\WinSxS" -Filter "$($imgArch)_microsoft-edge-webview_31bf3856ad364e35*" -Directory | Select-Object -ExpandProperty FullName > $null
+            if ($folderPath) {
+                & takeown /f "$folderPath" /r > $null
+                & icacls "$folderPath" /grant "$($adminGroup.Value):(F)" /T /C > $null
+                Remove-Item -Path "$folderPath" -Recurse -Force > $null
+            }
+        }
+        TakeOwnership("$workingDir\scratchdir\Windows\System32\Microsoft-Edge-Webview", "/r", "/T /C")
+        Remove-Item -Path "$workingDir\scratchdir\Windows\System32\Microsoft-Edge-Webview" -Recurse -Force > $null
+
+        Write-Progress -Status "Removing OneDrive"
+        TakeOwnership("$workingDir\scratchdir\Windows\System32\OneDriveSetup.exe", "/r", "/T /C")
+        Remove-Item -Path "$workingDir\scratchdir\Windows\System32\OneDriveSetup.exe" -Force > $null
+
+        Write-Progress -Activity "Loading image registry..." -Status ""
+        $registries | ForEach-Object { & reg load $_[0] $_[1] > $null }
+
+        Write-Progress -Activity "Modifying registry key permissions..." -Status "Enabling take ownership privilege"
+        Enable-Privilege SeTakeOwnershipPrivilege
+        Write-Progress -Status "Taking ownership of registry key"
+        $rkey = [Registry]::LocalMachine.OpenSubKey("zSOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tasks",[RegistryKeyPermissionCheck]::ReadWriteSubTree,[RegistryRights]::TakeOwnership)
+        $rACL = $rKey.GetAccessControl()
+        $rACL.SetOwner($adminGroup)
+        $rKey.SetAccessControl($regACL)
+        $rKey.Close()
+        Write-Progress -Status "Modifying Administrators group permissions on registry key"
+        $rKey = [Registry]::LocalMachine.OpenSubKey("zSOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tasks",[RegistryKeyPermissionCheck]::ReadWriteSubTree,[RegistryRights]::ChangePermissions)
+        $rACL = $rKey.GetAccessControl()
+        $rRule = New-Object RegistryAccessRule($adminGroup,"FullControl","ContainerInherit","None","Allow")
+        $rACL.SetAccessRule($rRule)
+        $rKey.SetAccessControl($rACL)
+        $rKey.Close()
+
+        Write-Progress -Activity "Modifying image registry..." -Status ""
+        $installRegKeys.GetEnumerator() | Foreach-Object {
+            Write-Progress -Status $_.Key
+            $_.Value | Foreach-Object {
+                if ($_[0] -eq "add") {
+                    & reg add $_[1] /v $_[2] /t $_[3] /d $_[4] /f > $null
+                }
+                elseif ($_[0] -eq "delete") {
+                    & reg delete $_[1] /f > $null
+                }
+                else {
+                    Write-Host "Warning: '$($_[0])' is an unknown registry operation."
+                }
+            }
+        }
+
+        Write-Progress -Activity "Unloading image registry..." -Status ""
+        $registries | ForEach-Object { & reg unload $_[0] > $null }
+
+        & dism /image:"$workingDir\scratchdir" /Cleanup-Image /StartComponentCleanup /ResetBase | ForEach-Object {
+            Write-Progress -Activity "Cleaning up install image..." -Status $_
+        }
+
+        & dism /unmount-image /mountdir:"$workingDir\scratchdir" /commit | ForEach-Object {
+            Write-Progress -Activity "Unmounting install image..." -Status $_
+        }
+
+        & dism /Export-Image /SourceImageFile:"$workingDir\sources\install.wim" /SourceIndex:$index /DestinationImageFile:"$workingDir\sources\install2.wim" /compress:max | ForEach-Object {
+            Write-Progress -Activity "Saving install image..." -Status $_
+        }
+        Remove-Item -Path "$workingDir\sources\install.wim" -Force > $null
+        Rename-Item -Path "$workingDir\sources\install2.wim" -NewName "install.wim" > $null
+
+        TakeOwnership("$workingDir\sources\boot.wim")
+        try { Set-ItemProperty -Path "$workingDir\sources\boot.wim" -Name IsReadOnly -Value $false -ErrorAction Stop > $null 2>&1}
+        catch {} # This block will catch the error and suppress it.
+
+        & dism /mount-image /imagefile:"$workingDir\sources\boot.wim" /index:2 /mountdir:"$workingDir\scratchdir" | ForEach-Object {
+            Write-Progress -Activity "Mounting boot image..." -Status $_
+        }
+
+        Write-Progress -Activity "Loading image registry..." -Status ""
+        $registries | ForEach-Object { & reg load $_[0] $_[1] > $null }
+
+        Write-Progress -Activity "Modifying image registry..." -Status ""
+        $bootRegKeys.GetEnumerator() | Foreach-Object {
+            Write-Progress -Status $_.Key
+            $_.Value | Foreach-Object {
+                if ($_[0] -eq "add") {
+                    & reg add $_[1] /v $_[2] /t $_[3] /d $_[4] /f > $null
+                }
+                elseif ($_[0] -eq "delete") {
+                    & reg delete $_[1] /f > $null
+                }
+                else {
+                    Write-Host "Warning: '$($_[0])' is an unknown registry operation."
+                }
+            }
+        }
+
+        Write-Progress -Activity "Unloading image registry..." -Status ""
+        $registries | ForEach-Object { & reg unload $_[0] > $null }
+
+        & dism /unmount-image /mountdir:"$workingDir\scratchdir" /commit | ForEach-Object {
+            Write-Progress -Activity "Unmounting boot image..." -Status $_
+        }
+
+        & dism /Export-Image /SourceImageFile:"$workingDir\sources\boot.wim" /SourceIndex:2 /DestinationImageFile:"$workingDir\sources\boot2.wim" /compress:max | ForEach-Object {
+            Write-Progress -Activity "Saving boot image..." -Status $_
+        }
+        Remove-Item -Path "$workingDir\sources\boot.wim" -Force > $null
+        Rename-Item -Path "$workingDir\sources\boot2.wim" -NewName "boot.wim" > $null
+
+        Write-Progress -Activity "Copying unattended file for bypassing MS account on OOBE..." -Status ""
+        Copy-Item -Path "$PSScriptRoot\autounattend.xml" -Destination "$workingDir\autounattend.xml" -Force > $null
+
+        Write-Progress -Activity "Removing temp directory..."
+        Remove-Item -Path "$workingDir\scratchdir" -Recurse -Force > $null
+
+        & "$PSScriptRoot\oscdimg.exe" -m -o -u2 -udfver102 "-bootdata:2#p0,e,b$workingDir\boot\etfsboot.com#pEF,e,b$workingDir\efi\microsoft\boot\efisys.bin" "$workingDir" "$PSScriptRoot\tiny11.iso" | ForEach-Object {
+            Write-Progress -Activity "Creating ISO image..." -Status $_
+        }
+
+        Write-Progress -Activity "Cleaning up..." -Status ""
+        Remove-Item -Path "$workingDir" -Recurse -Force > $null
     }
 
     while((Get-Job | Where-Object {$_.State -ne "Completed"}).Count -gt 0) {
@@ -114,6 +257,13 @@ function startBuild() {
         Write-Progress -ParentId 0 -Id 1 -Activity $($jobProgress.Activity) -Status $($jobProgress.StatusDescription)
         Start-Sleep -Milliseconds 100
     }
+
+    Clear-Host 
+    Write-Host "$([Console]::Title)$newLine${newLine}tiny11 ISO image has been created!${$newLine}Output path: '${$PSScriptRoot}\tiny11.iso'$newLine${$newLine}Press any key to exit...$newLine"
+    [Console]::ReadKey()
+
+    Stop-Transcript
+    exit
 }
 
 selectDrive
